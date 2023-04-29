987297d7faa7e135afd224148f9e63be34efcf28
MatArasim <arasimmateusz@gmail.com>
2023-04-29 16:50:50 +0200

    fix

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -12,12 +12,12 @@ class PostController extends Controller
 {
     public function hotSort()
     {
-        return PostResource::collection(Post::orderBy('rating', 'desc')->simplePaginate(15)->links());
+        return PostResource::collection(Post::orderBy('rating', 'desc')->paginate(15)->links());
     }
 
     public function newSort()
     {
-        return PostResource::collection(Post::orderBy('created_at', 'asc')->simplePaginate(15)->links());
+        return PostResource::collection(Post::orderBy('created_at', 'asc')->paginate(15)->links());
     }
 
     public function store(PostRequest $request, PostService $service)
